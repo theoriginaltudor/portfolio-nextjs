c093b081705e4b347ef403736503b9c3a3cb7583
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-06-24 19:59:32 +0200

    refactor: move AvatarWithShadow component to a new directory for better organization

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef } from "react";
-import AvatarWithShadow from "@/components/ui/AvatarWithShadow";
+import AvatarWithShadow from "@/components/AvatarWithShadow";
 
 export default function Home() {
   const formRef = useRef<HTMLFormElement>(null);
