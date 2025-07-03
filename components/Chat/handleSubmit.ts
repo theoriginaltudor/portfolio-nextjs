d77fc7e2c5e07f0995045150cc69d22eb6c0317f
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-07-03 12:25:33 +0200

    feat: update handleSubmit logic to include response schema and improve redirection

--- components/Chat/handleSubmit.ts
+++ components/Chat/handleSubmit.ts
@@ -1,21 +1,26 @@
 "use server";
 import { redirect } from "next/navigation";
 import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
+import { generateObject } from "ai";
+import z from "zod";
 export async function handleSubmit(formData: FormData) {
   const message = formData.get("message")?.toString() || "";
   const response = await getAIRoute(message);
-  redirect(`/${response.trimEnd()}`);
+  redirect(
+    `/${response.path.trimEnd()}?message=${encodeURIComponent(
+      response.response
+    )}`
+  );
 }
 
 const model = google("gemini-2.0-flash");
 
-const getAIRoute = async (message: string): Promise<string> => {
-  const { text } = await generateText({
+const getAIRoute = async (message: string): Promise<z.infer<typeof schema>> => {
+  const { object } = await generateObject({
     model,
+    schema,
     system:
-      "You're an assistant helping a user to navigate a portfolio website." +
-      "The only valid responses are 'contact', 'projects', 'project/1', 'project/2', 'project/3', nothing else.",
+      "You're an assistant helping a user to navigate a portfolio website.",
     messages: [
       {
         role: "user",
@@ -23,5 +28,18 @@ const getAIRoute = async (message: string): Promise<string> => {
       },
     ],
   });
-  return text;
+  return object;
 };
+
+const schema = z.object({
+  path: z
+    .string()
+    .describe(
+      "The path name to navigate to. Must be one of: 'contact', 'projects', 'project/1', 'project/2', 'project/3'."
+    ),
+  response: z
+    .string()
+    .describe(
+      "A short message to the user, such as: 'The information you requested can be found on this page.'"
+    ),
+});
