51a10ba4d01e76c6163fe5c29aca75e9573b3f22
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-07-06 15:28:48 +0200

    feat: implement Article component for enhanced markdown rendering in ProjectPage

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -3,9 +3,7 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import { slides } from "../db";
 import { unstable_ViewTransition as ViewTransition } from "react";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import rehypeSanitize from "rehype-sanitize";
+import Article from "@/features/workDescription/Article";
 
 interface ProjectPageProps {
   params: { id: string };
@@ -56,64 +54,9 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
           </div>
         </div>
       </div>
-      {/* Main content */}
-      <article className="max-w-2xl text-base w-full px-4 mt-8">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          rehypePlugins={[rehypeSanitize]}
-          components={components}
-        >
-          {project.longDescription}
-        </ReactMarkdown>
-      </article>
+      <Article className="max-w-2xl text-base w-full px-4 mt-8">
+        {project.longDescription}
+      </Article>
     </main>
   );
 }
-
-const components = {
-  h2: (props: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h2 className="text-3xl font-bold mt-6 mb-3" {...props} />
-  ),
-  a: (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
-    <a className="underline text-indigo-600 hover:text-indigo-800" {...props} />
-  ),
-  em: (props: React.HTMLAttributes<HTMLElement>) => (
-    <i className="text-red-400" {...props} />
-  ),
-  code: (props: {
-    inline?: boolean;
-    className?: string;
-    children?: React.ReactNode;
-  }) =>
-    props.inline ? (
-      <code className="px-1 bg-gray-200 rounded text-sm" {...props}>
-        {props.children}
-      </code>
-    ) : (
-      <pre
-        className="bg-gray-900 text-white p-4 rounded overflow-x-auto"
-        {...props}
-      >
-        <code>{props.children}</code>
-      </pre>
-    ),
-  ul: (props: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className="list-disc ml-6" {...props} />
-  ),
-  ol: (props: React.OlHTMLAttributes<HTMLOListElement>) => (
-    <ol className="list-decimal ml-6" {...props} />
-  ),
-  li: (props: React.LiHTMLAttributes<HTMLLIElement>) => (
-    <li className="mb-1" {...props} />
-  ),
-  table: (props: React.TableHTMLAttributes<HTMLTableElement>) => (
-    <table className="min-w-full border-collapse" {...props} />
-  ),
-  tr: (props: React.HTMLAttributes<HTMLTableRowElement>) => <tr {...props} />,
-  td: (props: React.TdHTMLAttributes<HTMLTableCellElement>) => (
-    <td className="border px-2 py-1" {...props} />
-  ),
-  th: (props: React.ThHTMLAttributes<HTMLTableCellElement>) => (
-    <th className="border px-2 py-1 font-bold" {...props} />
-  ),
-};
