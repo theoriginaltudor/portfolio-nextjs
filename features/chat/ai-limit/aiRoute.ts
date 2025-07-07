3a5c7fb13416f2e482b20e3dc60036716f29ce68
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-07-07 22:36:22 +0200

    feat: enhance AI route handling with embedding model and similarity sorting

--- features/chat/ai-limit/aiRoute.ts
+++ features/chat/ai-limit/aiRoute.ts
@@ -1,14 +1,28 @@
+import { slides } from "@/lib/db";
 import { google } from "@ai-sdk/google";
-import { generateObject } from "ai";
+import { cosineSimilarity, embed, embedMany, generateObject } from "ai";
 import z from "zod";
 
 export const model = google("gemini-2.0-flash");
+const embeddingModel = google.textEmbeddingModel("text-embedding-004");
+const values = slides.map((slide) => slide.title + " " + slide.description + " " + slide.longDescription);
+const { embeddings } = await embedMany({
+  model: embeddingModel,
+  values,
+})
+
+const vectorDatabase = embeddings.map(
+  (embedding, index) => ({
+    value: slides[index].id,
+    embedding,
+  }),
+);
 
 export const schema = z.object({
   path: z
     .string()
     .describe(
-      "The path name to navigate to. Must be one of: 'contact', 'project' (which is for all projects), 'project/1', 'project/2', 'project/3'."
+      "The path name to navigate to. Must be one of: 'contact', 'project' (which is for all projects)."
     ),
   response: z
     .string()
@@ -20,6 +34,33 @@ export const schema = z.object({
 export const getAIRoute = async (
   message: string
 ): Promise<{ object: z.infer<typeof schema>; tokens: number }> => {
+  const searchTerm = await embed({
+    model: embeddingModel,
+    value: message,
+  });
+  const entries = vectorDatabase.map((entry) => {
+    return {
+      value: entry.value,
+      similarity: cosineSimilarity(
+        entry.embedding,
+        searchTerm.embedding,
+      ),
+    };
+  });
+  const sortedEntries = entries.sort(
+    (a, b) => b.similarity - a.similarity,
+  );
+  console.log("Sorted entries:", sortedEntries);
+  if (sortedEntries[0].similarity > 0.8) {
+    const bestMatch = sortedEntries[0].value;
+    return {
+      object: {
+        path: bestMatch,
+        response: "The information you requested can be found on this page.",
+      },
+      tokens: 0, // No tokens used for this response
+    };
+  }
   const { object, usage } = await generateObject({
     model,
     schema,
