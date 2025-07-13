218403cb7defae2f5da88bbd41ee21260ad619d0
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-07-13 22:45:16 +0200

    feat: refactor ProjectPage to fetch project data from Supabase and update ProjectImageHeader to accept image prop

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { notFound } from "next/navigation";
-import { SlideData, slides } from "@/lib/db";
 import Article from "@/features/workDescription/Article";
 import ProjectImageHeader from "@/features/workDescription/ProjectImageHeader";
 import ProjectImageCarousel from "@/features/workDescription/ProjectImageCarousel";
 import Skills from "@/features/workDescription/Skills";
+import { supabase } from "@/lib/supabaseClient";
+import { Database } from "@/types/database.types";
 
 interface ProjectPageProps {
   params: Promise<{
@@ -17,31 +18,84 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
   const { slug } = await params;
   if (!slug) return notFound();
 
-  // Find project by slug property
-  const project: SlideData | undefined = slides.find(
-    (slide) => slide.slug === slug
-  );
-  if (!project) return notFound();
+  const { data: project, error: projectError } = await supabase
+    .from("articles")
+    .select("*")
+    .eq("slug", slug)
+    .single<Database["public"]["Tables"]["articles"]["Row"]>();
+  if (projectError) {
+    console.error("Error fetching project:", projectError);
+    return notFound();
+  }
+  if (!project) {
+    console.warn("Project not found for slug:", slug);
+    return notFound();
+  }
+  const { data: images, error: imagesError } = await supabase
+    .from("images")
+    .select("*")
+    .eq("article_id", project.id);
+
+  if (imagesError) {
+    console.error("Error fetching images:", imagesError);
+    return notFound();
+  }
+  if (!images) {
+    console.warn("No images found for project:", project.id);
+  }
+
+  const { data: skillsArticles, error: skillsArticlesError } = await supabase
+    .from("articles_skills")
+    .select("*")
+    .eq("article_id", project.id);
+
+  if (skillsArticlesError) {
+    console.error("Error fetching skills articles:", skillsArticlesError);
+    return notFound();
+  }
+
+  if (!skillsArticles) {
+    console.warn("No skills articles found for project:", project.id);
+  }
+
+  const { data: skills, error: skillsError } = await supabase
+    .from("skills")
+    .select("*")
+    .in(
+      "id",
+      skillsArticles.map((s) => s.skill_id)
+    );
+  if (skillsError) {
+    console.error("Error fetching skills:", skillsError);
+    return notFound();
+  }
+  if (!skills) {
+    console.warn("No skills found for project:", project.id);
+  }
 
   return (
     <main className="flex flex-col items-center flex-1 w-full">
-      <ProjectImageHeader project={project} id={project.id} />
+      <ProjectImageHeader
+        project={project}
+        id={project.id}
+        image={images[0]?.path}
+      />
 
-      <Skills skills={project.skills} />
+      <Skills skills={skills.map((s) => s.name)} />
 
       <Article className="max-w-2xl text-base w-full px-4 mt-8">
-        {project.longDescription}
+        {project.long_description}
       </Article>
       {/* Carousel of project images */}
-      {project.image && project.image.length > 1 && (
-        <ProjectImageCarousel images={project.image} />
+      {images && images.length > 1 && (
+        <ProjectImageCarousel images={images.map((img) => img.path)} />
       )}
     </main>
   );
 }
 
-export async function generateStaticParams() {
-  return slides.map((slide) => ({
-    slug: slide.slug,
-  }));
-}
+// export async function generateStaticParams() {
+//   return slides.map((slide) => ({
+//     slug: slide.slug,
+//   }));
+// }
