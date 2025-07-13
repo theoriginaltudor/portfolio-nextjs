3340f0db8bc823f0ebc2db1a24f015b862b81a09
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-07-13 20:28:43 +0200

    feat: add Supabase client and database types for improved type safety

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,7 @@
+import { Database } from '@/types/database.types'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.DB_CONNECTION_STRING as string
 const supabaseAnonKey = process.env.DB_API_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
