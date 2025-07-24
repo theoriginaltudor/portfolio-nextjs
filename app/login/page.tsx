b98a5230b68830b7683d8d8b4b02c3ca065a7f28
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-07-24 08:42:14 +0200

    feat: implement LoginPage with styled components and add Label component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,30 +1,46 @@
+import { Input } from "@/components/ui/input";
 import { loginUser } from "./actions";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 
 export default async function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        required
-        autoComplete="email"
-        pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
-        minLength={5}
-        maxLength={254}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        required
-        minLength={6}
-        maxLength={128}
-        autoComplete="current-password"
-      />
-      <button formAction={loginUser}>Log in</button>
+    <form
+      className="flex flex-col gap-6 p-4 justify-center items-center mx-auto"
+      action={loginUser}
+    >
+      <div className="flex gap-2 w-full">
+        <Label htmlFor="email" className="flex-1 justify-end">
+          Email:
+        </Label>
+        <Input
+          className="w-[200px]"
+          id="email"
+          name="email"
+          type="email"
+          required
+          autoComplete="email"
+          pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
+          minLength={5}
+          maxLength={254}
+        />
+      </div>
+      <div className="flex gap-2 w-full">
+        <Label htmlFor="password" className="flex-1 justify-end">
+          Password:
+        </Label>
+        <Input
+          className="w-[200px]"
+          id="password"
+          name="password"
+          type="password"
+          required
+          minLength={6}
+          maxLength={128}
+          autoComplete="current-password"
+        />
+      </div>
+      <Button type="submit">Log in</Button>
     </form>
   );
 }
